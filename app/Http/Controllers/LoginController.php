5bb6fe6d6faf7c7c29dd51bf245a88f60aa0906b
hvtk <hvtk1968@gmail.com>
2021-12-23 19:12:50 +0100

    Added the login-view

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Hash;
+use Session;
+use App\Models\User;
 
 class LoginController extends Controller
 {
