7357ca090200617e229bbe34a80a0dd8307eb634
hvtk <hvtk1968@gmail.com>
2022-01-12 16:07:05 +0100

    Added link bootstrap css in layout blades

--- resources/views/auth-layout.blade.php
+++ resources/views/auth-layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Receptenboek">
     <meta name="author" content="Henk van 't Kruijs">
+    <!-- Added bootstrap 4 to Laravel 8 -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css" />
     <title>@yield('title')</title>
    
     <style>
