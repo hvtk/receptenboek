a3030417f5b1f27b0e843337f5b667acd5abf0fd
hvtk <hvtk1968@gmail.com>
2022-01-15 12:30:26 +0100

    Deleted old blades and routes.

--- resources/views/authenticate/dashboard.blade.old.php
+++
@@ -1,9 +0,0 @@
-@extends('auth-layout')
-
-@section('title', 'Logout Page')
-
-@section('content')
-<div class="form-signin" style="max-widht: 500px">
-  <a href="{{ route('auth.logout') }}">Sign-out</a>
-</div> 
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,23 +20,11 @@ Route::get('/', function() {
     return view('welcome');
 });
 
-Route::get('/login', [AuthController::class, 'index'])->name('auth.login');
-Route::post('/custom-signin', [AuthController::class, 'createSignin'])->name('auth.custom.signin');
-
-Route::get('/register', [AuthController::class, 'signup'])->name('auth.register');
-Route::post('/creater-user', [AuthController::class, 'customSignup'])->name('auth.user.registration');
-
-Route::get('/dashboard', [AuthController::class, 'dashboardView'])->name('auth.dashboard');
-Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
-
 Route::resource('accounts', AccountController::class);
-
 
 Route::post('/authenticate/save',[MainController::class, 'save'])->name('authenticate.save');
 Route::post('/authenticate/check',[MainController::class, 'check'])->name('authenticate.check');
 Route::get('/authenticate/logout',[MainController::class, 'logout'])->name('authenticate.logout');
-
-
 
 Route::group(['middleware'=>['AuthenticateCheck']], function() {
     
